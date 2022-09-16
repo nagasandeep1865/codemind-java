e7fffd69dcc4339493cef1ae61e7727392b7fd16
naga sandeep <96776344+nagasandeep1865@users.noreply.github.com>
2022-09-16 11:40:08 +0530

    import java.util.*; class Tenet {     public static void main(String args[])     {         char a;         Scanner sc=new Scanner(System.in);         a=sc.nextLine().charAt(0);         System.out.printf("%d",(int)a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Tenet
+{
+    public static void main(String args[])
+    {
+        char a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextLine().charAt(0);
+        System.out.printf("%d",(int)a);
+    }
+}
