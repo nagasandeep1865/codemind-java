7950fc111729f0f1ecc7df18a6552b8853046403
naga sandeep <96776344+nagasandeep1865@users.noreply.github.com>
2022-09-16 10:34:56 +0530

    import java.util.*; class Tenet {     public static void main(String args[])     {         int x,y,z;         Scanner sc=new Scanner(System.in);         x=sc.nextInt();         y=sc.nextInt();         z=x+y;         System.out.println(z);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Tenet
+{
+    public static void main(String args[])
+    {
+        int x,y,z;
+        Scanner sc=new Scanner(System.in);
+        x=sc.nextInt();
+        y=sc.nextInt();
+        z=x+y;
+        System.out.println(z);
+    }
+}
