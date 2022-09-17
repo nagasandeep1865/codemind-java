73618b791b5b9446ae940c10db3f30ea3eaa70fe
naga sandeep <96776344+nagasandeep1865@users.noreply.github.com>
2022-09-17 09:58:44 +0530

    import java.util.*; class Tenet {     public static void main(String args[])     {         int x,y;         Scanner sc=new Scanner(System.in);         x=sc.nextInt();         y=sc.nextInt();         if(x%2==0  && (x>0 || y%2==0))         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Can_share_equally?.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Tenet
+{
+    public static void main(String args[])
+    {
+        int x,y;
+        Scanner sc=new Scanner(System.in);
+        x=sc.nextInt();
+        y=sc.nextInt();
+        if(x%2==0  && (x>0 || y%2==0))
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
