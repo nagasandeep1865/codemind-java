b646530deeec2964d2944f4158bb7bf0a3b82c6c
naga sandeep <96776344+nagasandeep1865@users.noreply.github.com>
2022-09-23 11:40:56 +0530

    import java.util.*; class Tenet {     public static void main(String args[])     {         int a,n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         a=(n*(n+1)/2);         System.out.println(a);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Tenet
+{
+    public static void main(String args[])
+    {
+        int a,n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        a=(n*(n+1)/2);
+        System.out.println(a);
+    }
+}
