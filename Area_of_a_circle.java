d25dc4cddb343edbab9f9ee0d1bc376339b22850
naga sandeep <96776344+nagasandeep1865@users.noreply.github.com>
2022-09-23 10:47:11 +0530

    import java.util.*; class Tenet {     public static void main(String args[])     {         float a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextFloat();         b=(float)(3.14*(a*a));         System.out.println(b);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Tenet
+{
+    public static void main(String args[])
+    {
+        float a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextFloat();
+        b=(float)(3.14*(a*a));
+        System.out.println(b);
+    }
+}
