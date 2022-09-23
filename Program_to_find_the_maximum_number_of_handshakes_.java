ef6615552ed23a18a11d0386cfbeb4ea1b2fa93a
naga sandeep <96776344+nagasandeep1865@users.noreply.github.com>
2022-09-23 11:13:58 +0530

    import java.util.*; class Tenet {     public static void main(String args[])     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=a*(a-1)/2;         System.out.println(b);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Tenet
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=a*(a-1)/2;
+        System.out.println(b);
+    }
+}
