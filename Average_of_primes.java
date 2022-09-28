39e645c2258a570258e716809ede5148bf6998f8
naga sandeep <96776344+nagasandeep1865@users.noreply.github.com>
2022-09-28 21:52:18 +0530

    import java.util.Scanner; class Prime {     public static boolean prime(int n)     {         if(n<2)         {             return false;         }         for(int i=2;i<n;i++)         {             if(n%i==0)             {                 return false;             }         }         return true;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[];         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         int s=0,c=0;         for(int ele:x)         {             if(prime(ele))             {                 s+=ele;                 c++;             }         }         float f=(float)s/c;         System.out.format("%.2f",f);
        }
    }

---
+++ Average_of_primes.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class Prime
+{
+    public static boolean prime(int n)
+    {
+        if(n<2)
+        {
+            return false;
+        }
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[];
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        int s=0,c=0;
+        for(int ele:x)
+        {
+            if(prime(ele))
+            {
+                s+=ele;
+                c++;
+            }
+        }
+        float f=(float)s/c;
+        System.out.format("%.2f",f);
+    
+    }
+}
