91e8b985d2f162e27f39b9b2dcb2055728cafaba
naga sandeep <96776344+nagasandeep1865@users.noreply.github.com>
2022-10-12 15:38:46 +0530

    import java.util.Scanner; class Array {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         int[] arr2=new int[100];         int l=0;         arr2[l++]=x[0];        for(i=0;i<n;i++)        {            int c=0;            for(int k=0;k<l;k++)            {                if(arr2[k]==x[i])                {                    c++;                }            }            if(c==0)            {                arr2[l++]=x[i];            }        }        int y=0;        for(int p=0;p<l;p++)        {            int s=0;            for(i=0;i<n;i++)            {                if(arr2[p]==x[i])                {                    s++;                }            }            if(s==arr2[p])            {                System.out.print(arr2[p]+" ");                y++;            }
           }
           if(y==0)
           {
               System.out.println("-1");
           }
           
        }
    }

---
+++ Elements_same_as_count.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+class Array
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        int[] arr2=new int[100];
+        int l=0;
+        arr2[l++]=x[0];
+       for(i=0;i<n;i++)
+       {
+           int c=0;
+           for(int k=0;k<l;k++)
+           {
+               if(arr2[k]==x[i])
+               {
+                   c++;
+               }
+           }
+           if(c==0)
+           {
+               arr2[l++]=x[i];
+           }
+       }
+       int y=0;
+       for(int p=0;p<l;p++)
+       {
+           int s=0;
+           for(i=0;i<n;i++)
+           {
+               if(arr2[p]==x[i])
+               {
+                   s++;
+               }
+           }
+           if(s==arr2[p])
+           {
+               System.out.print(arr2[p]+" ");
+               y++;
+           }
+           
+       }
+       if(y==0)
+       {
+           System.out.println("-1");
+       }
+       
+    }
+}
