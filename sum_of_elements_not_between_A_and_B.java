64e0ced695b96e5a7a6cc18697a3f46dfa6e1355
naga sandeep <96776344+nagasandeep1865@users.noreply.github.com>
2022-10-12 15:54:54 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         int a=sc.nextInt();         int b=sc.nextInt();         int s=0;         for(i=0;i<n;i++)         {             if(x[i]<a || x[i]>b)             {                 /*System.out.print(x[i]+" ");*/                 s+=x[i];             }         }         System.out.print(s);     } }

---
+++ sum_of_elements_not_between_A_and_B.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int s=0;
+        for(i=0;i<n;i++)
+        {
+            if(x[i]<a || x[i]>b)
+            {
+                /*System.out.print(x[i]+" ");*/
+                s+=x[i];
+            }
+        }
+        System.out.print(s);
+    }
+}
