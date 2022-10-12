e3ed13e902e9b789a68bc99ce3bd1edde92e9ddb
naga sandeep <96776344+nagasandeep1865@users.noreply.github.com>
2022-10-12 22:32:07 +0530

    import java.util.*; class Elements {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         int s=0;         for(int ele:x)         {             s+=ele;         }         int res=s/n;         int c=0;         for(int ele:x)         {             if(ele<=res)             {                 c++;             }         }         System.out.print(c);     } }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Elements
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        int s=0;
+        for(int ele:x)
+        {
+            s+=ele;
+        }
+        int res=s/n;
+        int c=0;
+        for(int ele:x)
+        {
+            if(ele<=res)
+            {
+                c++;
+            }
+        }
+        System.out.print(c);
+    }
+}
