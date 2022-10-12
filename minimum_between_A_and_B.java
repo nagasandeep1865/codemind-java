a3e847cb88d88cf62ebeba75c4144db0261e45c7
naga sandeep <96776344+nagasandeep1865@users.noreply.github.com>
2022-10-12 14:29:55 +0530

    import java.util.*; class Min {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         int a=sc.nextInt();         int b=sc.nextInt();         int[] q=new int[100];         int l=0;         for(i=0;i<n;i++)         {             if(x[i]>=a && x[i]<=b)             {                 q[l++]=x[i];             }         }         int min=q[0],c=0;         for(int j=0;j<l;j++)         {             if(min>q[j])             {                 min=q[j];             }             c++;         }         if(c==0)         {             System.out.print("-1");         }         else         {             System.out.print(min);         }
        }
    }

---
+++ minimum_between_A_and_B.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Min
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int[] q=new int[100];
+        int l=0;
+        for(i=0;i<n;i++)
+        {
+            if(x[i]>=a && x[i]<=b)
+            {
+                q[l++]=x[i];
+            }
+        }
+        int min=q[0],c=0;
+        for(int j=0;j<l;j++)
+        {
+            if(min>q[j])
+            {
+                min=q[j];
+            }
+            c++;
+        }
+        if(c==0)
+        {
+            System.out.print("-1");
+        }
+        else
+        {
+            System.out.print(min);
+        }
+        
+    }
+}
